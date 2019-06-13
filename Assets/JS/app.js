c1486a6324ea5b733840d9426b4b8eef1f69122a
Gold Dust <48570104+ashley-blue@users.noreply.github.com>
2019-06-12 23:14:03 -0600

    game is working, buttons have been added. needs timer and some reset functions

--- Assets/JS/app.js
+++ Assets/JS/app.js
@@ -1,41 +1,110 @@
-const quizContainer = document.getElementById('quiz');
-const resultsContainer = document.getElementById('results');
-const submitButton = document.getElementById('submit');
+var questions = ["Which character says,'Damn good coffee!'?","Where does 'Twin Peaks' take place?","Which character does Dana Ashbrook play?"];
+var answer= ["Dale Cooper","Sheriff Truman","Benjamin Horne","Washington","Canada","Oregon","Bobby","Leo","Ed"];
+var correct = 0;
+var incorrect = 0;
+var myAnswer = 0;
+var questionAnswer = 0;
+var counter = 30;
 
-function buildQuiz(){}
 
-function showResults(){}
+$("#start").click(function () {
 
-buildQuiz();
+    chooseQuestion();
 
-submitButton.addEventListener('click', showResults);
+});
 
-const questions = [
-    {
-        question: "Which character says,'Damn good coffee!'?",
-        answers: {
-            a: "Benjamin Horne",
-            b: "Dale Cooper",
-            c: "Sheriff Truman",
-        },
-        correctAnswer: "b"
-    },
-    {
-        question: "Where does 'Twin Peaks' take place?",
-        answers: {
-            a: "Oregon",
-            b: "Canada",
-            c: "Washington",
-        },
-        correctAnswer: "c"
-    },
-    {
-        question: "Which character does Dana Ashbrook play?",
-        answers: {
-            a: "Bobby",
-            b: "Leo",
-            c: "Ed",
-        },
-        correctAnswer: "a",
-    },
-];
+$("#answer1").click(function (){
+    myAnswer = 1;
+    checkAnswer ();
+});
+
+$("#answer2").click(function () {
+    myAnswer = 2;
+    checkAnswer ();
+});
+
+$("#answer3").click(function () {
+    myAnswer = 3;
+    checkAnswer ();
+});
+
+
+
+function chooseQuestion(){
+
+    var randomQuestion = Math.floor(Math.random() * 3);
+    console.log(randomQuestion);
+
+    if(randomQuestion === 0){
+
+        $("#quiz").html(questions[0]);
+
+        $("#dispAnswer1").html(answer[1]);
+
+        $("#dispAnswer2").html(answer[0]);
+
+        $("#dispAnswer3").html(answer[2]);
+
+        questionAnswer = 2;
+    }
+
+    if(randomQuestion === 1){
+        
+        $("#quiz").html(questions[1]);
+
+        $("#dispAnswer1").html(answer[3]);
+
+        $("#dispAnswer2").html(answer[5]);
+
+        $("#dispAnswer3").html(answer[4]);
+
+        questionAnswer = 1;
+
+    }
+
+    if(randomQuestion === 2){
+        
+        $("#quiz").html(questions[2]);
+    
+        $("#dispAnswer1").html(answer[8]);
+
+        $("#dispAnswer2").html(answer[7]);
+
+        $("#dispAnswer3").html(answer[6]);
+
+        questionAnswer = 3;
+    }
+
+}
+
+function checkAnswer(){
+
+    if(questionAnswer === myAnswer){
+
+        correct ++;
+        $("#Correct").html(correct);
+        chooseQuestion();
+
+
+    }
+
+
+    else{
+
+        incorrect ++;
+        $("#Incorrect").html(incorrect);
+        chooseQuestion();
+
+
+    }
+
+}
+
+//need to complete timer
+
+var interval = setInterval(function () {
+    counter--;
+    if(counter <= 0) {
+        clearInterval(interval);
+        $('#timer').html("<h3>Time is up!</h3>");
+} 1000});
